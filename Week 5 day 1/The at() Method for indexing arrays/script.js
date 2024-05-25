ac06b1dd0fee18876ee50cfc70814917fdbad469
Sambit Kumar Sutar <36398016+sambitsutar1999@users.noreply.github.com>
2024-05-25 01:10:21 +0000

    done

---
+++ Week 5 day 1/The at() Method for indexing arrays/script.js
@@ -0,0 +1,12 @@
+let arr = [23,14,98];
+
+console.log(arr[1]);
+console.log(arr.at(1));
+
+console.log(arr[arr.length -1]);
+console.log(arr.slice(-1[0]));
+console.log(arr.at(-1));
+console.log(arr.at(-2));
+
+console.log("Arjit".at[0]);
+console.log("Arjit".at(-1));
