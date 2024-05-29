824da9a80ff7ae49b9062d19d35a6944086b1785
Sambit Kumar Sutar <36398016+sambitsutar1999@users.noreply.github.com>
2024-05-29 02:55:03 +0000

    done

---
+++ WEEK 5 DAY 4/Using Numeric Separators for Readability in JavaScript.js
@@ -0,0 +1,16 @@
+const diameter = 246_456_000_000;
+console.log(diameter);
+
+const price = 349_99;
+console.log(price);
+
+const transferFee = 15_99;
+const transferFee2 = 1_599;
+
+
+const PI = 3.14_15;
+console.log(PI)
+
+
+console.log(Number('23000'));  // NaN
+console.log(parseInt('230_00'));
