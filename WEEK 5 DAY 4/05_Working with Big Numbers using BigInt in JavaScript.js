3e7c13e5c478a046228b5032f13e264cb606d59a
Sambit Kumar Sutar <36398016+sambitsutar1999@users.noreply.github.com>
2024-05-29 09:38:36 +0000

    done

--- WEEK 5 DAY 4/05_Working with Big Numbers using BigInt in JavaScript.js
+++ WEEK 5 DAY 4/05_Working with Big Numbers using BigInt in JavaScript.js
@@ -1,1 +1,32 @@
 // Working with Big Numbers
+console.log(2**53 +1);
+console.log(Number.MAX _SAFE_INTEZER);
+console.log(2**53 +1);
+console.log(2**53 +2);
+console.log(2**53 +3);
+console.log(2**53 +4);
+console.log(2**53 +5);
+
+console.log(657566757656656456845648n);
+console.log(BigInt(65674545665));
+
+console.log(3000n + 1000n);
+console.log(656757656566454554n * 10000n);
+
+
+const huge = 6545454564564654567n;
+const num = 23;
+
+console.log(huge * num);
+
+
+//Exceptions
+console.log(20n > 15);
+console.log(20n === 20);
+console.log(typeof 20n);
+console.log(typeof 20);
+console.log(20n == '20');
+
+//Divisions
+console.log(11n/3n)
+console.log(11/3)
