81f3b6e075988802eb93972250425a97454b1691
Sambit Kumar Sutar <36398016+sambitsutar1999@users.noreply.github.com>
2024-06-04 11:17:24 +0000

    done

---
+++ WEEK 6/W6 D3 OOPS/7.Introduction to ES6 Classes.js
@@ -0,0 +1,34 @@
+//  Class Expressions
+// const personcl = class{
+
+// }
+
+// Class Declaration
+class personcl {
+    constructor(firstName.birthYear){
+    this.firstName = firstName;
+    this.birthYear = birthYear
+    }
+    calcAge(){
+        console.log(2024 - this.birthYear)
+    }
+
+    greet(){
+        console.log("Hey There!")
+    }
+}
+
+const sambit = new personcl('Sambit',1999);
+console.log(rohit);
+
+//console.log(rohit.__proto__);
+console.log(rohit.__proto__ === personcl.prototype)
+
+rohit calcAge();
+
+personcl.prototype.greet = 
+
+rohit.greet();
+
+//Classes are not hoisted
+//Classes are also first class citizen in js
