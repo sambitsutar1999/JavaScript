ce5296e493bad98e570c306aae8a528d2c2a2850
Sambit Kumar Sutar <36398016+sambitsutar1999@users.noreply.github.com>
2024-06-24 14:58:46 +0000

    done

--- WEEK 9/W9 D2/script.js
+++ WEEK 9/W9 D2/script.js
@@ -1,35 +1,51 @@
+// $(function(){
+//     //Fading --> removed slowly,added slowly
+
+
+
+//     // $(".myh1").fadeOut(3000) --> class name
+//     // $("#myh1").fadeIn(2000) --> Id
+
+//     // $("h1").fadeOut(5000) --> tag Name
+
+//     // $(".box3").fadeOut(3000) //GREEN BOX 3(REMOVED)
+//     // $(".box3").fadeIn(4000) //GREEN BOX 3 and DUPULICATE GREEN BOX 3(Comeback)
+//     // $(".box3").fadeToggle(3000)
+
+//     //Opacity : 0 (completely removed) & 1 (visible)
+
+//     // $(".box1").fadeTo(4000, 0.2)
+
+
+// // Hode and Show Method
+
+//     // $(".box1").hide(4000)
+//     // $(".box2").hide(5000)
+//     // $(".box3").hide(6000)
+
+//     // $(".box1").show(1000)
+//     // $(".box2").show(2000)
+//     // $(".box3").show(3000)
+
+
+
+//     $(".box3").toggle(3000)
+
+
+
+// })
+
+
 $(function(){
-    //Fading --> removed slowly,added slowly
+    // $(".box1").slideUp(5000)
+    // $(".box2").slideUp(5000)
+    // $(".box3").slideUp(5000)
 
+    // $(".box1").slideDown(5000)
+    // $(".box2").slideDown(5000)
+    // $(".box3").slideDown(5000)
 
-
-    // $(".myh1").fadeOut(3000) --> class name
-    // $("#myh1").fadeIn(2000) --> Id
-
-    // $("h1").fadeOut(5000) --> tag Name
-
-    // $(".box3").fadeOut(3000) //GREEN BOX 3(REMOVED)
-    // $(".box3").fadeIn(4000) //GREEN BOX 3 and DUPULICATE GREEN BOX 3(Comeback)
-    // $(".box3").fadeToggle(3000)
-
-    //Opacity : 0 (completely removed) & 1 (visible)
-
-    // $(".box1").fadeTo(4000, 0.2)
-
-
-// Hode and Show Method
-
-    // $(".box1").hide(4000)
-    // $(".box2").hide(5000)
-    // $(".box3").hide(6000)
-
-    // $(".box1").show(1000)
-    // $(".box2").show(2000)
-    // $(".box3").show(3000)
-
-
-
-    $(".box3").toggle(3000)
+    $(".box3").slideToggle(5000)
 
 
 
