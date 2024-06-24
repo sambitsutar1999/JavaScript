17b1511cbb40ab89495031c5fad8628dea270b16
Sambit Kumar Sutar <36398016+sambitsutar1999@users.noreply.github.com>
2024-06-24 01:26:47 +0000

    done

---
+++ WEEK 9/W9 D2/script.js
@@ -0,0 +1,14 @@
+$(function(){
+    //Fading --> removed slowly,added slowly
+
+
+
+    // $(".myh1").fadeOut(3000)
+    // $("#myh1").fadeIn(2000)
+
+    // $("h1").fadeOut(5000)
+
+    // $(".box3").fadeOut(3000)
+    // $(".box3").fadeIn(4000)
+    $(".box3").fadeToggle(3000)
+})
