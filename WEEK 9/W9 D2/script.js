903434547a6a9bf47bb7dac684d796c4a5cb47f5
Sambit Kumar Sutar <36398016+sambitsutar1999@users.noreply.github.com>
2024-06-24 01:29:53 +0000

    done

--- WEEK 9/W9 D2/script.js
+++ WEEK 9/W9 D2/script.js
@@ -3,12 +3,12 @@ $(function(){
 
 
 
-    // $(".myh1").fadeOut(3000)
-    // $("#myh1").fadeIn(2000)
+    // $(".myh1").fadeOut(3000) --> class name
+    // $("#myh1").fadeIn(2000) --> Id
 
-    // $("h1").fadeOut(5000)
+    // $("h1").fadeOut(5000) --> tag Name
 
-    // $(".box3").fadeOut(3000)
-    // $(".box3").fadeIn(4000)
+    // $(".box3").fadeOut(3000) //GREEN BOX 3(REMOVED)
+    // $(".box3").fadeIn(4000) //GREEN BOX 3 and DUPULICATE GREEN BOX 3(Comeback)
     $(".box3").fadeToggle(3000)
 })
