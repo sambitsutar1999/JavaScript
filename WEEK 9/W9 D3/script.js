4be07a5fe6a21025f71a0f5db3dc28dcc9c27a62
Sambit Kumar Sutar <36398016+sambitsutar1999@users.noreply.github.com>
2024-06-25 14:51:52 +0000

    done

---
+++ WEEK 9/W9 D3/script.js
@@ -0,0 +1,21 @@
+$(function(){
+    // $("#myh1").next().css({backgroundColor: "blue"})
+
+    // $("#myh1").nextAll().css({"backgroundColor": "silver","color": "orange"})
+
+    // $("#myul").nextAll().css({"backgroundColor": "silver","color": "pink"})
+
+    // $("#myp").prev().css({"backgroundColor": "silver","color": "green"})
+
+    // $("#myul").prevAll().css({"backgroundColor": "silver","color": "cyan"})
+
+    // $("#myul").before("<h2>Waaaaaaoooooooo</h2>")
+
+    // $("#myh1").after("<h2>Hmmmmmmmmmmmmmmm</h2>")
+
+    // $("li").parent().css({"color": "green", "fontSize": "40px"})
+
+    // $("li").parents().css({"color": "green", "fontSize": "40px"})
+
+    // $("#myul").children().css({"color": "green", "fontSize": "40px"})
+})
