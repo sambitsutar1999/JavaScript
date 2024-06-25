28258038f13da71465238126730e329ba2265d9d
Sambit Kumar Sutar <36398016+sambitsutar1999@users.noreply.github.com>
2024-06-25 17:09:31 +0000

    done

---
+++ WEEK 9/W9 D3/Adding Elements to the DOM/script.js
@@ -0,0 +1,25 @@
+$(function(){
+    // $("#myol").append("<li>MANGENTA</li><li>PURPLE</li>")
+
+    // $("#myol").prepend("<li>Cyan</li><li>Golden</li>")
+
+    // $("#myol").appendTo("<li>Cyan</li><li>Golden</li>") ---->INVALID SYNTAX
+
+    // $(selector).append/prepend(content)
+
+    // $(content).appendTo(selector)
+
+    // $("<li>Cyan</li><li>Golden</li>").appendTo("#myol")
+
+    // $("<li>Ocean</li><li>Dark Red</li>").prependTo("#myol")
+
+    // $("#myol").after("<h1>I am from after Method</h1>")
+
+    // $("#myol").before("<h1>I am from before Method</h1>")
+
+    // $("<h1>I am from insertAfter Method</h1>").insertAfter("#myol")
+
+    // $("<h1>I am from insertBefore Method</h1>").insertBefore("#myol")
+
+
+})
