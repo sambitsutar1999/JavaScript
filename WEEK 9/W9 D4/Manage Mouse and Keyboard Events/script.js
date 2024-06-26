9dc446daa95e265d8caaf22dd8d47ace9dafbcfd
Sambit Kumar Sutar <36398016+sambitsutar1999@users.noreply.github.com>
2024-06-26 17:42:33 +0000

    done

---
+++ WEEK 9/W9 D4/Manage Mouse and Keyboard Events/script.js
@@ -0,0 +1,79 @@
+
+// MOUSE EVENTS
+//   Syntax   - mouseup()
+            //    -mousedown()
+            //    -mouseenter()
+            //    -mouseleave()
+
+// ---------------Mouse UP------------------
+
+// $(function(){
+//     $("button").mouseup(function(){
+//         $("div").css({"backgroundColor": "black"})
+//     })
+// })
+
+
+//------------------Mouse Down-------------------------------
+// $(function(){
+//     $("button").mousedown(function(){
+//         $("div").css({"backgroundColor": "black"})
+//     })
+// })
+
+
+
+//---------------------------Mouse Enter--------------------------
+
+// $(function(){
+//     $("button").mouseenter(function(){
+//         $("div").css({"backgroundColor": "cyan"})
+//     })
+// })
+
+
+//--------------------------mouseleave-------------------
+
+// $(function(){
+//     $("button").mouseleave(function(){
+//         $("div").css({"backgroundColor": "pink"})
+//     })
+// })
+
+
+
+
+//                               KEYBOARD EVENTS
+
+// 1.keyup()
+// 2.keydown()
+// 3.keypress()
+
+
+//                             keyup
+
+// $(function(){
+//     // $(function(){
+//     // $("button").keyup(function(){
+//     //     $("div").css({"backgroundColor": "pink"})
+//     // })
+//     // })
+
+
+// //                            keydown                   
+
+// // $(function(){
+// //     $("button").keydown(function(){
+// //         $("div").css({"backgroundColor": "pink"})
+// //     })
+// // })
+
+
+// //                          keypress
+
+// $(function(){
+//     $("button").keypress(function(){
+//         $("div").css({"backgroundColor": "black"})
+//     })
+// })
+// })
