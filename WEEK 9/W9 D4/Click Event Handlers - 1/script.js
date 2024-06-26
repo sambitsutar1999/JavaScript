b9a9c1f4fcbcf12c2cb76cb45cbd7286b6938c7f
Sambit Kumar Sutar <36398016+sambitsutar1999@users.noreply.github.com>
2024-06-26 02:17:49 +0000

    done

---
+++ WEEK 9/W9 D4/Click Event Handlers - 1/script.js
@@ -0,0 +1,11 @@
+$(function(){
+    // $("button").click(function(){
+    //     $(".container").fadeTo(3000, "0.2")
+    //     $(".container").fadeTo(3000, "1")
+    // })
+
+    $("button").dblclick(function(){
+        // $(".container").fadeTo(3000, "0.2")
+        $(".container").fadeTo(3000, "0.3")
+    })
+})
---
+++ WEEK 9/W9 D4/Click Event Handlers - 2/script.js
@@ -0,0 +1,8 @@
+$(function(){
+
+    $(".login_form").hide()
+    $("#change_button").click(function(){
+        $(".login_form, .register_form").toggle()
+
+    })
+})
